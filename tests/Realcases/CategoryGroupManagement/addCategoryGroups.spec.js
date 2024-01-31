5f9c0e12d3a4e077f447c6e1aac0e193c2ba48d7
Diego Maia <ldgmaia@gmail.com>
2024-01-31 12:49:55 -0500

    Fix: Added more Cat Groups and added a delay to close the browser

--- tests/Realcases/CategoryGroupManagement/addCategoryGroups.spec.js
+++ tests/Realcases/CategoryGroupManagement/addCategoryGroups.spec.js
@@ -1,11 +1,11 @@
-import { test, expect } from "@playwright/test";
+import { test } from "@playwright/test";
 
 test("test", async ({ page }) => {
   await page.goto("http://localhost:5173/");
   await page.goto("http://localhost:5173/#/sign-in?redirectUrl=/");
   await page.getByPlaceholder("User Name").click();
   await page.getByPlaceholder("User Name").fill("admin@rbms.com");
-  await page.getByPlaceholder("Password").click();
+  await page.getByPlaceholder("User Name").press("Tab");
   await page.getByPlaceholder("Password").fill("password");
   await page.getByRole("button", { name: "Sign In" }).click();
   await page.getByRole("link", { name: "Category Groups Management" }).click();
@@ -13,11 +13,20 @@ test("test", async ({ page }) => {
   await page.getByPlaceholder("Category Group Name").click();
   await page.getByPlaceholder("Category Group Name").fill("Computing Devices");
   await page.getByRole("button", { name: "Submit" }).click();
-
   await page.getByRole("button", { name: "Create Category Group" }).click();
+  await page.getByPlaceholder("Category Group Name").click();
   await page.getByPlaceholder("Category Group Name").click();
   await page
     .getByPlaceholder("Category Group Name")
     .fill("Parts and Accessories");
   await page.getByRole("button", { name: "Submit" }).click();
+  await page.getByRole("button", { name: "Create Category Group" }).click();
+  await page.getByPlaceholder("Category Group Name").click();
+  await page.getByPlaceholder("Category Group Name").fill("Other devices");
+  await page.getByRole("button", { name: "Submit" }).click();
+  await page.getByRole("button", { name: "Create Category Group" }).click();
+  await page.getByPlaceholder("Category Group Name").click();
+  await page.getByPlaceholder("Category Group Name").fill("Service");
+  await page.getByRole("button", { name: "Submit" }).click();
+  await page.waitForTimeout(500);
 });
